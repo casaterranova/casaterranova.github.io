bdd6a3059e9dafb7e2c0c405dd203d93f02af22a
Renato Fabbri <renato.fabbri@gmail.com>
2019-06-17 04:59:37 -0300

    teste

--- js/demo/demo.js
+++ js/demo/demo.js
@@ -18,6 +18,33 @@ $(function () {
     'imgs/cozinha.jpeg'
   ]
 
+let imgsall = [
+  'banheiro.jpeg',
+  'banheiro2.jpeg',
+  'cozinha.jpeg',
+  'cozinha2.jpeg',
+  'cozinha3.jpeg',
+  'fachada.jpeg',
+  'fachada2.jpeg',
+  'frente.jpeg',
+  'frente2.jpeg',
+  'fundo.jpeg',
+  'fundo2.jpeg',
+  'fundo3.jpeg',
+  'lavanderia.jpeg',
+  'quarto.jpeg',
+  'quarto2.jpeg',
+  'sala.jpeg',
+  'sala2.jpeg',
+  'sala3.jpeg',
+  'sala4.jpeg',
+  'sala5.jpeg'
+]
+
+let imgs_best = [
+]
+
+
   // Load demo images from flickr:
   var carouselLinks = []
   var linksContainer = $('#links')
